3624cae7140a5867d39121fc42cc673310daa36f
sannyralph <sannylauren@gmail.com>
2024-03-03 17:04:35 -0500

    style update

--- src/components/Navbar/freeShiping.jsx
+++ src/components/Navbar/freeShiping.jsx
@@ -13,7 +13,7 @@ const Promotions = () => {
       currentSlide = (currentSlide + 1) % slides.length;
       setTimeout(() => {
         slides[currentSlide].style.animation = "slide-in 1s forwards";
-      }, 1000); // Delay for 1 second
+      }, 1000); 
     };
 
     slideInNext();
