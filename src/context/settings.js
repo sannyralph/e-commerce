3eed772a753e792447d95415285468281a50a90d
sannyralph <sannylauren@gmail.com>
2024-03-16 09:57:18 -0400

    context setting added for product management

---
+++ src/context/settings.js
@@ -0,0 +1,19 @@
+import { useState, createContext }  from "react";
+
+const  SettingsContext = createContext()
+
+export const SettingsProvider = ({children}) => {
+    const [product, setProduct ] = useState([])
+    const [count, setCount ] = useState(0)
+    const [cart, setCart ] = useState([])
+    const [searchItem, setSearchItem ] = useState("")
+
+    const values = {product, setProduct, count, setCount, cart, setCart, searchItem, setSearchItem}
+        
+        return <SettingsContext.Provider value={values}>
+                {children}
+        </SettingsContext.Provider>
+} 
+
+export default SettingsProvider
+
